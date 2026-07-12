ec5e9d25a338833532d5071528c4e2f34391e68e
bharathalaparath <96810316+bharathalaparath@users.noreply.github.com>
2026-07-12 17:41:07 +0530

    Added Allure report generation and upload to Github actions

--- src/test/java/tests/GoogleSearchTest.java
+++ src/test/java/tests/GoogleSearchTest.java
@@ -13,6 +13,11 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.time.Duration;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import io.qameta.allure.Epic;
 
 public class GoogleSearchTest {
 
@@ -32,6 +37,11 @@ public class GoogleSearchTest {
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
+
+    @Epic("Login Module")
+    @Story("Valid Login")
+    @Description("Verify that a valid user can log in successfully")
+    @Severity(SeverityLevel.CRITICAL)
 
     @Test
     public void loginTest() {
