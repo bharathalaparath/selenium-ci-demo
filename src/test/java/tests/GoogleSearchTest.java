b4b7747046fe251273b609af75152a7900a8a7d2
bharathalaparath <96810316+bharathalaparath@users.noreply.github.com>
2026-07-12 13:45:57 +0530

    Initial Selenium CI demo project

---
+++ src/test/java/tests/GoogleSearchTest.java
@@ -0,0 +1,67 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.time.Duration;
+
+public class GoogleSearchTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--incognito");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @Test
+    public void loginTest() {
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Enter username
+        WebElement username = wait.until(
+                ExpectedConditions.elementToBeClickable(By.id("username"))
+        );
+        username.sendKeys("tomsmith");
+
+        // Enter password
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+
+        // Click login
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        // Verify success message
+        WebElement message = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("flash"))
+        );
+
+        System.out.println("Message: " + message.getText());
+        Assert.assertTrue(message.getText().contains("You logged into a secure area!"));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) driver.quit();
+    }
+}
